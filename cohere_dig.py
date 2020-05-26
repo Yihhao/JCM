4017d27fb97e26c3d9c817372ae354bae36649c9
Yihao <zxas12584163@gmail.com>
2020-05-26 20:52:36 +0800

    restore file and create ppt to restore fig

--- cohere_dig.py
+++ cohere_dig.py
@@ -7,22 +7,12 @@ N = 20
 z = 1.2
 psi0 = coherent(N, z)
 fig, ax = plot_fock_distribution(psi0)
-# ax.set_xticks([1.5, 2.5], minor=True)
 ax.set_xticks(np.arange(N+1), minor=False)
-# plot_wigner_fock_distribution(psi0)
 plt.savefig('./fig/coherent.png')
 plt.show()
 
-
-xvec = np.linspace(-5, 5, 200)
-fig, ax = plt.subplots(figsize=(6, 6))
-W = wigner(psi0, xvec, xvec)
-ax.contourf(xvec, xvec, W, 100, norm=mpl.colors.Normalize(-.125, .125), cmap=plt.get_cmap('RdBu'))
+plot_wigner(psi0)
 plt.xlabel('X', fontsize=16)
 plt.ylabel('P', fontsize=16)
-plt.title('Phase space \n z = %s' % z)
-plt.tight_layout()
 plt.savefig('./fig/wigner_coh.png')
 plt.show()
-
-parfor()
